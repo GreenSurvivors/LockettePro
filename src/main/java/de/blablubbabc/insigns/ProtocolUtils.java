46ed3b50e5c65c87cb72d40ca03f78b89801a1e5
Joshi1999 <joshi1999@yahoo.de>
2022-01-14 14:37:02 +0100

    Update to 1.18

---
+++ src/main/java/de/blablubbabc/insigns/ProtocolUtils.java
@@ -0,0 +1,237 @@
+package de.blablubbabc.insigns;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.List;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.InternalStructure;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.reflect.EquivalentConverter;
+import com.comphenix.protocol.wrappers.BlockPosition;
+import com.comphenix.protocol.wrappers.nbt.NbtBase;
+import com.comphenix.protocol.wrappers.nbt.NbtCompound;
+import com.comphenix.protocol.wrappers.nbt.NbtFactory;
+
+/**
+ * Utilities for reading and writing packet contents.
+ */
+public final class ProtocolUtils {
+
+    private ProtocolUtils() {
+    }
+
+    public static final class Packet {
+
+        private Packet() {
+        }
+
+        public static final class TileEntityData {
+
+            private TileEntityData() {
+            }
+
+            public static boolean isTileEntityDataPacket(PacketContainer packet) {
+                if (packet == null) return false;
+                if (packet.getType() != PacketType.Play.Server.TILE_ENTITY_DATA) return false;
+                return true;
+            }
+
+            // TODO Update this to use the packet's BlockEntityType field to identify sign updates. However, ProtocolLib
+            // does not seem to offer some kind of converter for BlockEntityType yet (e.g. one that maps to a BlockState
+            // class, or some other closest Bukkit representation).
+            @Deprecated
+            public static boolean isUpdateSignPacket(PacketContainer packet) {
+                assert isTileEntityDataPacket(packet);
+                throw new UnsupportedOperationException("Outdated operation");
+            }
+
+            public static BlockPosition getBlockPosition(PacketContainer packet) {
+                assert isTileEntityDataPacket(packet);
+                return packet.getBlockPositionModifier().read(0);
+            }
+
+            public static void setBlockPosition(PacketContainer packet, BlockPosition blockPosition) {
+                assert isTileEntityDataPacket(packet);
+                packet.getBlockPositionModifier().write(0, blockPosition);
+            }
+
+            public static NbtCompound getTileEntityData(PacketContainer packet) {
+                assert isTileEntityDataPacket(packet);
+                return (NbtCompound) packet.getNbtModifier().read(0);
+            }
+
+            public static void setTileEntityData(PacketContainer packet, NbtCompound tileEntityData) {
+                assert isTileEntityDataPacket(packet);
+                packet.getNbtModifier().write(0, tileEntityData);
+            }
+        }
+
+        public static final class MapChunk {
+
+            private MapChunk() {
+            }
+
+            public static boolean isMapChunkPacket(PacketContainer packet) {
+                if (packet == null) return false;
+                if (packet.getType() != PacketType.Play.Server.MAP_CHUNK) return false;
+                return true;
+            }
+
+            public static List<InternalStructure> getTileEntitiesInfo(PacketContainer packet) {
+                assert isMapChunkPacket(packet);
+                return packet.getStructures().read(0).getLists(TileEntityInfo.CONVERTER).read(0);
+            }
+
+            public static void setTileEntitiesInfo(PacketContainer packet, List<InternalStructure> tileEntitiesInfo) {
+                assert isMapChunkPacket(packet);
+                packet.getStructures().read(0).getLists(TileEntityInfo.CONVERTER).write(0, tileEntitiesInfo);
+            }
+
+            public static int getChunkX(PacketContainer packet) {
+                return packet.getIntegers().read(0);
+            }
+
+            public static int getChunkZ(PacketContainer packet) {
+                return packet.getIntegers().read(1);
+            }
+
+            @SuppressWarnings("unchecked")
+            public static final class TileEntityInfo {
+
+                private static final EquivalentConverter<InternalStructure> CONVERTER;
+                static {
+                    try {
+                        Field converterField = InternalStructure.class.getDeclaredField("CONVERTER");
+                        converterField.setAccessible(true);
+                        CONVERTER = (EquivalentConverter<InternalStructure>) converterField.get(null);
+                    } catch (ReflectiveOperationException e) {
+                        throw new RuntimeException("Could not get the tile entity info converter!", e);
+                    }
+                }
+
+                private static Constructor<?> CONSTRUCTOR; // Lazily initialized
+
+                private TileEntityInfo() {
+                }
+
+                // The x and y coordinates are stored compactly as: ((X & 15) << 4) | (Z & 15)
+                private static int getPackedXZ(InternalStructure tileEntityInfo) {
+                    return tileEntityInfo.getIntegers().read(0);
+                }
+
+                public static int getLocalX(InternalStructure tileEntityInfo) {
+                    return (getPackedXZ(tileEntityInfo) >> 4);
+                }
+
+                public static int getLocalZ(InternalStructure tileEntityInfo) {
+                    return (getPackedXZ(tileEntityInfo) & 0xf);
+                }
+
+                public static int getY(InternalStructure tileEntityInfo) {
+                    return tileEntityInfo.getIntegers().read(1);
+                }
+
+                private static Object getTileEntityType(InternalStructure tileEntityInfo) {
+                    return tileEntityInfo.getModifier().read(2);
+                }
+
+                // Can be null
+                public static NbtCompound getNbt(InternalStructure tileEntityInfo) {
+                    NbtBase<?> nbt = tileEntityInfo.getNbtModifier().read(0);
+                    if (nbt == null) return null;
+                    return NbtFactory.asCompound(nbt);
+                }
+
+                public static InternalStructure cloneWithNewNbt(InternalStructure tileEntityInfo, NbtCompound nbt) {
+                    if (CONSTRUCTOR == null) {
+                        for (Constructor<?> constructor : tileEntityInfo.getHandle().getClass().getDeclaredConstructors()) {
+                            // We are looking for the only constructor with 4 parameters:
+                            if (constructor.getParameterCount() == 4) {
+                                constructor.setAccessible(true);
+                                CONSTRUCTOR = constructor;
+                                break;
+                            }
+                        }
+                        if (CONSTRUCTOR == null) {
+                            throw new RuntimeException("Could not find the tile entity info constructor!");
+                        }
+                    }
+
+                    Object packedXZ = getPackedXZ(tileEntityInfo);
+                    Object y = getY(tileEntityInfo);
+                    Object tileEntityType = getTileEntityType(tileEntityInfo);
+                    Object nmsNbt = nbt.getHandle();
+
+                    try {
+                        Object instance = CONSTRUCTOR.newInstance(packedXZ, y, tileEntityType, nmsNbt);
+                        return CONVERTER.getSpecific(instance);
+                    } catch (ReflectiveOperationException e) {
+                        throw new RuntimeException("Could not invoke the tile entity info constructor!", e);
+                    }
+                }
+            }
+        }
+    }
+
+    public static final class TileEntity {
+
+        private TileEntity() {
+        }
+
+        public static String getId(NbtCompound tileEntityData) {
+            assert tileEntityData != null;
+            return tileEntityData.getString("id");
+        }
+
+        public static int getX(NbtCompound tileEntityData) {
+            assert tileEntityData != null;
+            return tileEntityData.getInteger("x");
+        }
+
+        public static int getY(NbtCompound tileEntityData) {
+            assert tileEntityData != null;
+            return tileEntityData.getInteger("y");
+        }
+
+        public static int getZ(NbtCompound tileEntityData) {
+            assert tileEntityData != null;
+            return tileEntityData.getInteger("z");
+        }
+
+        public static final class Sign {
+
+            private Sign() {
+            }
+
+            public static boolean isTileEntitySignData(NbtCompound tileEntityData) {
+                // TODO Some packets omit the TileEntity type id from the NBT data and instead store it separately in
+                // form of a BlockEntityType. Once ProtocolLib offers some kind of converter to represent
+                // BlockEntityType, all checks for whether some packet's tile entity data is a sign should be based on
+                // the BlockEntityType, and not the NBT data.
+                // String id = getId(tileEntityData);
+                // return id.equals("minecraft:sign");
+                return tileEntityData.containsKey("GlowingText");
+            }
+
+            public static String[] getText(NbtCompound tileEntitySignData) {
+                assert tileEntitySignData != null;
+                String[] lines = new String[4];
+                for (int i = 0; i < 4; i++) {
+                    String rawLine = tileEntitySignData.getString("Text" + (i + 1));
+                    lines[i] = rawLine == null ? "" : rawLine;
+                }
+                return lines;
+            }
+
+            public static void setText(NbtCompound tileEntitySignData, String[] lines) {
+                assert tileEntitySignData != null;
+                assert lines != null;
+                assert lines.length == 4;
+                for (int i = 0; i < 4; i++) {
+                    tileEntitySignData.put("Text" + (i + 1), lines[i]);
+                }
+            }
+        }
+    }
+}
--- src/main/java/me/crafter/mc/lockettepro/DependencyProtocolLib.java
+++ src/main/java/me/crafter/mc/lockettepro/DependencyProtocolLib.java
@@ -1,20 +1,18 @@
 package me.crafter.mc.lockettepro;
 
+import de.blablubbabc.insigns.ProtocolUtils;
+
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.WrappedChatComponent;
+import com.comphenix.protocol.events.*;
 import com.comphenix.protocol.wrappers.nbt.NbtCompound;
 import com.comphenix.protocol.wrappers.nbt.NbtFactory;
+
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 public class DependencyProtocolLib {
 
@@ -41,10 +39,24 @@ public class DependencyProtocolLib {
             @Override
             public void onPacketSending(PacketEvent event) {
                 PacketContainer packet = event.getPacket();
-                if (packet.getIntegers().read(0) != 9) return;
-                NbtCompound nbtcompound = (NbtCompound) packet.getNbtModifier().read(0);
-                onSignSend(event.getPlayer(), nbtcompound);
-                packet.getNbtModifier().write(0, nbtcompound);
+
+                NbtCompound signData = ProtocolUtils.Packet.TileEntityData.getTileEntityData(packet);
+
+                if (signData == null || !ProtocolUtils.TileEntity.Sign.isTileEntitySignData(signData)) {
+                    return;
+                }
+
+                String[] rawLines = ProtocolUtils.TileEntity.Sign.getText(signData);
+
+                if (onSignSend(rawLines)) {
+                    PacketContainer outgoingPacket = event.getPacket().shallowClone();
+
+                    NbtCompound outgoingSignData = replaceSignData(signData, rawLines);
+
+                    ProtocolUtils.Packet.TileEntityData.setTileEntityData(outgoingPacket, outgoingSignData);
+
+                    event.setPacket(outgoingPacket);
+                }
             }
         });
     }
@@ -55,36 +67,80 @@ public class DependencyProtocolLib {
             @Override
             public void onPacketSending(PacketEvent event) {
                 PacketContainer packet = event.getPacket();
-                List<?> tileentitydatas = packet.getSpecificModifier(List.class).read(0);
-                for (Object tileentitydata : tileentitydatas) {
-                    NbtCompound nbtcompound = NbtFactory.fromNMSCompound(tileentitydata);
-                    if (!"minecraft:sign".equals(nbtcompound.getString("id"))) continue;
-                    onSignSend(event.getPlayer(), nbtcompound);
-                }
-                packet.getSpecificModifier(List.class).write(0, tileentitydatas);
 
+                // Only replace the outgoing packet if needed:
+                PacketContainer outgoingPacket = null;
+                List<InternalStructure> outgoingTileEntitiesInfo = null;
+
+                List<InternalStructure> tileEntitiesInfo = ProtocolUtils.Packet.MapChunk.getTileEntitiesInfo(packet);
+                for (int index = 0, size = tileEntitiesInfo.size(); index < size; index++) {
+                    InternalStructure tileEntityInfo = tileEntitiesInfo.get(index);
+                    NbtCompound tileEntityData = ProtocolUtils.Packet.MapChunk.TileEntityInfo.getNbt(tileEntityInfo);
+                    if (tileEntityData == null) {
+                        continue;
+                    }
+
+                    // Check if the tile entity is a sign:
+                    if (!ProtocolUtils.TileEntity.Sign.isTileEntitySignData(tileEntityData)) {
+                        continue;
+                    }
+
+                    String[] rawLines = ProtocolUtils.TileEntity.Sign.getText(tileEntityData);
+
+                    if (onSignSend(rawLines)) {
+                        if (outgoingPacket == null) {
+                            outgoingPacket = packet.shallowClone();
+                            outgoingTileEntitiesInfo = new ArrayList<>(tileEntitiesInfo);
+                        }
+
+                        NbtCompound outgoingSignData = replaceSignData(tileEntityData, rawLines);
+
+                        InternalStructure newTileEntityInfo = ProtocolUtils.Packet.MapChunk.TileEntityInfo.cloneWithNewNbt(tileEntityInfo, outgoingSignData);
+                        outgoingTileEntitiesInfo.set(index, newTileEntityInfo);
+                    }
+                }
+
+                if (outgoingPacket != null) {
+                    ProtocolUtils.Packet.MapChunk.setTileEntitiesInfo(outgoingPacket, outgoingTileEntitiesInfo);
+
+                    event.setPacket(outgoingPacket);
+                }
             }
         });
     }
 
-    public static void onSignSend(Player player, NbtCompound nbtcompound) {
-        String raw_line1 = nbtcompound.getString("Text1");
-        if (LocketteProAPI.isLockStringOrAdditionalString(Utils.getSignLineFromUnknown(raw_line1))) {
-            // Private line
-            String line1 = Utils.getSignLineFromUnknown(nbtcompound.getString("Text1"));
-            if (LocketteProAPI.isLineExpired(line1)) {
-                nbtcompound.put("Text1", WrappedChatComponent.fromText(Config.getLockExpireString()).getJson());
-            } else {
-                nbtcompound.put("Text1", WrappedChatComponent.fromText(Utils.StripSharpSign(line1)).getJson());
-            }
-            // Other line
-            for (int i = 2; i <= 4; i++) {
-                String line = Utils.getSignLineFromUnknown(nbtcompound.getString("Text" + i));
-                if (Utils.isUsernameUuidLine(line)) {
-                    nbtcompound.put("Text" + i, WrappedChatComponent.fromText(Utils.getUsernameFromLine(line)).getJson());
-                }
-            }
+    private static NbtCompound replaceSignData(NbtCompound previousSignData, String[] newSignText) {
+        NbtCompound newSignData = NbtFactory.ofCompound(previousSignData.getName());
+
+        // Copy the previous tile entity data (shallow copy):
+        for (String key : previousSignData.getKeys()) {
+            newSignData.put(key, previousSignData.getValue(key));
         }
+
+        // Replace the sign text:
+        ProtocolUtils.TileEntity.Sign.setText(newSignData, newSignText);
+
+        return newSignData;
     }
 
+    public static boolean onSignSend(String[] rawLines) {
+        if (LocketteProAPI.isLockStringOrAdditionalString(Utils.getSignLineFromUnknown(rawLines[0]))) {
+            // Private line
+            String line1 = Utils.getSignLineFromUnknown(rawLines[0]);
+            if (LocketteProAPI.isLineExpired(line1)) {
+                rawLines[0] = "{\"text\":\"" + Config.getLockExpireString() + "\"}";
+            } else {
+                rawLines[0] = "{\"text\":\"" + Utils.StripSharpSign(line1) + "\"}";
+            }
+            // Other line
+            for (int i = 1; i < 4; i++) {
+                String line = Utils.getSignLineFromUnknown(rawLines[i]);
+                if (Utils.isUsernameUuidLine(line)) {
+                    rawLines[i] = "{\"text\":\"" + Utils.getUsernameFromLine(line) + "\"}";
+                }
+            }
+            return true;
+        }
+        return false;
+    }
 }
